c065b7d1604e3d40d52b9dac6cc11dda33ca4f70
Frank Rousseau <frank.rousseau@cozycloud.cc>
2016-03-09 13:14:00 +0100

    Fix slug generation when name contains `.`

--- server/lib/base_konnector.js
+++ server/lib/base_konnector.js
@@ -18,7 +18,7 @@ module.exports = {
    * level.
    */
   createNew: function (konnector) {
-    var slug = slugify(konnector.name).replace(/-/g, '_');
+    var slug = slugify(konnector.name).replace(/(-|\.)/g, '_');
     var logger = printit({
       prefix: konnector.name,
       date: true
