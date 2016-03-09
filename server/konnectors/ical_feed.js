d9afad7af316f5467c95d9a1e7814ad5ff9e1ba4
Frank Rousseau <frank.rousseau@cozycloud.cc>
2016-03-09 01:00:41 +0100

    Konnector ical feed: Fix full day import

--- server/konnectors/ical_feed.js
+++ server/konnectors/ical_feed.js
@@ -93,6 +93,12 @@ function saveEvents(requiredFields, entries, data, next) {
 
   async.eachSeries(entries.events, (icalEvent, done) => {
     icalEvent.tags = [requiredFields.calendar];
+    console.log(icalEvent);
+    if(icalEvent.start.indexOf('T00:00:00+00:00') &&
+       icalEvent.end.indexOf('T00:00:00+00:00')) {
+         icalEvent.start = icalEvent.start.substring(0, 10);
+         icalEvent.end = icalEvent.end.substring(0, 10);
+    }
     Event.createOrUpdate(icalEvent, (err, cozyEvent, changes) => {
       if (err) {
         connector.logger.error(err);
