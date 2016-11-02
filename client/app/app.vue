edff4b4b12f5509cdd79e240b1d4581b49d09e95
Noëlie Andrieu <noelieandrieu@gmail.com>
2016-11-02 14:40:55 +0100

    [markup] call SVG into pug files

--- client/app/app.vue
+++ client/app/app.vue
@@ -5,14 +5,17 @@
             ul
                 li
                     router-link(to="/discovery")
+                        svg: use(:xlink:href="require('./assets/sprites/icon-discovery.svg')")
                         | {{ 'my_accounts discovery title' | t }}
 
                 li
                     router-link(to="/category")
+                        svg: use(:xlink:href="require('./assets/sprites/icon-category.svg')")
                         | {{ 'my_accounts category title' | t }}
 
                 li
                     router-link(to="/connected")
+                        svg: use(:xlink:href="require('./assets/sprites/icon-connected.svg')")
                         | {{ 'my_accounts connected title' | t }}
 
         router-view
