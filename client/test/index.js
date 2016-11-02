d77a2ca99a95e517ff4e3470fc4b736414e1fe81
Noëlie Andrieu <noelieandrieu@gmail.com>
2016-11-02 16:35:44 +0100

    [test] add missing modules

--- client/test/index.js
+++ client/test/index.js
@@ -1,5 +1,33 @@
-'use strict';
+"use strict";
 
-describe('(beta) test', () => {
-    it('...', () => { });
-});
+import jsdomGlobal from 'jsdom-global'
+let jsdom = jsdomGlobal()
+
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import VuePolyglot from '../app/plugins/vue-polyglot'
+
+Vue.use(VueRouter)
+Vue.use(VuePolyglot)
+
+import App from '../app/app.vue'
+
+
+describe('Routes', () => {
+
+  describe('Discovery', () => {
+    it('Should be default route', () => { })
+    it('Should be into markup', () => { })
+    it('Should redirect to `/discovery`', () => { })
+  })
+
+  describe('Categories', () => {
+    it('Should be into markup', () => { })
+    it('Should redirect to `/category`', () => { })
+  })
+
+  describe('Connected', () => {
+    it('Should be into markup', () => { })
+    it('Should redirect to `/connected`', () => { })
+  })
+})
