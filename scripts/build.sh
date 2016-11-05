0fb27a86a233a1178c2c02c191567f30dca0d373
Lucas Verney <phyks@phyks.me>
2016-11-05 05:51:57 -0400

    Dev tools fixes (#560)
    * Dev tools fixes
    * Fix split on `:` in dev tools which was preventing from setting a field
    to a URL value.
    See https://github.com/cozy-labs/konnectors/issues/547#issue-185478387.
    * Use JSON description to set fields through the dev tools, which is a
    more robust and reliable way to describe the fields values, without
    parsing errors due to commas or colons.
    * Fix `konnector_hash` and dev tools to be able to use ES6 konnectors as
    well as JS folder import as a package.
    * Introduce a new script to build server part only, which can be called
    from `npm run build:server`.
    * Convert dev tools code to ES6, using
    [`decaffeinate`](https://github.com/decaffeinate/decaffeinate).
    * Fix the dev tools to look for modules in `build` directory rather than
    `server` directory directly. This makes it compatible with
    ES6/Coffee/JS, at the expense of having to run a `npm run build:server`
    before.
    * Add a `npm run dev_tools` endpoint to ease launch of dev tools.
    * `vendor` is not a Konnector model attribute and then should not be in
    the ouput of `--list` command in the dev tools.
    * `--change` should not allow one to set fields not declared in the
    connector definition.
    Fixes #547.
    Fixes #553.
    * Update .gitignore to ignore Vagrant files

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,32 +1,28 @@
 #!/usr/bin/env bash
+PATH="./node_modules/.bin:$PATH"
 
 echo "Clean previous server"
 rm -rf build/server && mkdir -p build/server
 echo "Previous server cleaned."
 
-echo "Build server files..."
-./node_modules/.bin/coffee -cb --output build/server server
-./node_modules/.bin/coffee -cb --output build/ server.coffee
-./node_modules/.bin/babel ./server/konnectors -d build/server/konnectors
-./node_modules/.bin/babel ./server/lib -d build/server/lib
-echo "Server built."
+source `dirname $0`/build_server.sh
 
 echo "Clean previous client build..."
 rm -rf build/client && mkdir -p build/client/app
 echo "Previous client cleaned."
 
 echo "Build entry point..."
-./node_modules/.bin/jade ./client/index.jade -c --out ./build/client/
+jade ./client/index.jade -c --out ./build/client/
 echo "var jade = require('jade/runtime');module.exports=" | \
     cat - ./build/client/index.js > ./build/client/index.js.tmp
 mv ./build/client/index.js.tmp ./build/client/index.js
 echo "Entry point built."
 
 echo "Build locales..."
-./node_modules/.bin/coffee -cb --output build/client/app/locales ./client/app/locales
+coffee -cb --output build/client/app/locales ./client/app/locales
 echo "Locales built."
 
 echo "Build client..."
-cd client/ && npm i && ./node_modules/.bin/brunch build --production && cd ..
+cd client/ && npm i && brunch build --production && cd ..
 cp -R client/public build/client/
 echo "Client built."
