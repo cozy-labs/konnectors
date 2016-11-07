b57d2f2d91337397415f6396d051e784913093a3
m4dz <code@m4dz.net>
2016-11-07 16:00:36 +0100

    [chore] remove useless files

--- client/app/components/examples/foo.vue
+++
@@ -1,8 +0,0 @@
-<template lang="pug">
-    div
-        | Foo
-        // - Test translation through vue-polyglot filter, wiht vars & counters
-        p {{ 'file error' | t }}
-        p {{ 'notification prefix' | t({name: 'foobar'}) }}
-        p {{ 'notification commits' | t({smart_count: 8}) }}
-</template>
