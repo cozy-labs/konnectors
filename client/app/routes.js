e5ba63595d804ddc204bcb5676b2d4c43abde92f
m4dz <code@m4dz.net>
2016-11-07 17:48:04 +0100

    [fix] redirect root instead of aliasing it

--- client/app/routes.js
+++ client/app/routes.js
@@ -2,18 +2,19 @@ import DiscoveryList from './components/discovery_list'
 import CategoryList from './components/category_list'
 import ConnectedList from './components/connected_list'
 
-export default [
-  {
-    path: '/category',
-    component: CategoryList
-  },
-  {
-    path: '/discovery',
-    alias: '/',
-    component: DiscoveryList
-  },
-  {
-    path: '/connected',
-    component: ConnectedList
-  }
-]
+export default [{
+  path: '/category',
+  component: CategoryList
+},
+{
+  path: '/discovery',
+  component: DiscoveryList
+},
+{
+  path: '/connected',
+  component: ConnectedList
+},
+{
+  path: '/',
+  redirect: 'discovery'
+}]
