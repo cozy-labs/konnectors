899599a714e5f3f9acdb3b673769d0637efb854d
Noëlie Andrieu <noelieandrieu@gmail.com>
2016-11-08 13:30:40 +0100

    [improvment] add name to routes

--- client/app/routes.js
+++ client/app/routes.js
@@ -4,22 +4,27 @@ import ConnectedList from './components/connected_list'
 import SampleDialog from './components/examples/dialog'
 
 export default [{
+  name: 'categoryList',
   path: '/category',
   component: CategoryList
 },
 {
+  name: 'dialog',
   path: '/category/sample',
   component: SampleDialog
 },
 {
+  name: 'discoveryList',
   path: '/discovery',
   component: DiscoveryList
 },
 {
+  name: 'connectedList',
   path: '/connected',
   component: ConnectedList
 },
 {
+  name: 'default',
   path: '/',
   redirect: '/discovery'
 }]
