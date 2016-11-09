22d8efd1e49a970eb3d25dc01586e5e81596fc76
Noëlie Andrieu <noelieandrieu@gmail.com>
2016-11-09 14:59:45 +0100

    [feature] add submit behaviour

--- client/app/components/examples/dialog.vue
+++ client/app/components/examples/dialog.vue
@@ -5,43 +5,99 @@
         include ../../templates/category_list.pug
 
     block dialog
-        div(aria-hidden='false', role='dialog')
-            div(role='separator', v-on:click="closeWindow")
+        div(v-bind:aria-hidden="hidden", role="dialog")
+            div(role='separator', v-on:click="onClose")
             .wrapper
                 div(role='contentinfo')
-                    header
-                        a(v-on:click="closeWindow", title='close')
-                            | Close
+                    header(v-bind:style="headerStyles")
+                        a(v-on:click="onClose", title='close') Close
+                        block dialog_header
+                            p dialog_header free content
 
-                    p hello
+                    div
+                        block dialog_content
+                            p dialog free content
 
                     footer
-                        button(v-on:click="submit", title='submit')
-                            | Next
+                        block dialog_footer
+                            button(v-on:click="onClose", title='cancel')
+                                | cancel
+                            button(v-on:click="next", title='submit')
+                                | next
 
-        </div>
 </template>
-
 
 <script>
     export default {
+        data () {
+            return {
+                hidden: false,
+                header: {
+                    'background-image': 'url(test0.png)'
+                },
+                routes: {
+                    success: { name: 'dialogSuccess' },
+                    error: { name: 'dialogError' },
+                    close: { name: 'categoryList' }
+                }
+            }
+        },
+        computed: {
+            headerStyles: {
+                get () {
+                    let styles
+                    for (let prop in this.header) {
+                        const value = `${prop}: ${this.header[prop]}`
+                        if (styles === undefined) styles = value
+                        else styles += `; ${value}`
+                    }
+                    return styles
+                }
+            }
+        },
         methods: {
-            closeWindow () {
-                this.$router.push({ name: 'categoryList'})
+            validate () {
+                // Add specific validation here
+                // return error if exists
+                // otherwise return nothing
             },
-            submit () {
-                const url = ''
-                const data = {}
 
-                // Error
-                //this.next(true)
-
-                // Success
-                this.next(null, true)
+            submit (complete) {
+                // Add submit behavior here
+                if (complete !== undefined) complete(this.data)
+                else complete('Missing `onSuccess` callback')
             },
-            next (err, result) {
-                if (err) this.$router.push({ name: 'dialogError'})
-                else this.$router.push({ name: 'dialogSuccess'})
+
+            onClose () {
+                this.$router.push(this.routes.close)
+            },
+
+            onError (err) {
+                const route = Object.assign(this.routes.error, {
+                    params: err
+                })
+                this.$router.push(route)
+                return false
+            },
+
+            onSuccess (result) {
+                const route = Object.assign(this.routes.success, {
+                    params: result
+                })
+                this.$router.push(route)
+                return true
+            },
+
+            next () {
+                // Validation client-side
+                const err = this.validate()
+                if (err) return this.onError(err)
+
+                // Validation server-side
+                this.submit((err, result) => {
+                    if (err) return this.onError(err)
+                    else return this.onSuccess(result)
+                })
             }
         }
     }
