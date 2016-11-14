39d06a0c955710cad4c3f78ee9261f19fd4a0a4a
Noëlie Andrieu <noelieandrieu@gmail.com>
2016-11-14 16:33:29 +0100

    [review] remove useless markup (and update CSS)

--- client/app/components/dialog.vue
+++ client/app/components/dialog.vue
@@ -2,21 +2,22 @@
     div(v-bind:aria-hidden="hidden", role="dialog")
         div(role='separator', v-on:click="onClose")
         .wrapper
-            header(v-bind:style="headerStyles")
-                a(v-on:click="onClose", title='close') Close
-                block dialog_header
-                    p dialog_header free content
+            div(role='contentinfo')
+                header(v-bind:style="headerStyles")
+                    a(v-on:click="onClose", title='close') Close
+                    block dialog_header
+                        p dialog_header free content
 
-            div
-                block dialog_content
-                    p dialog free content
+                div
+                    block dialog_content
+                        p dialog free content
 
-            footer
-                block dialog_footer
-                    button(v-on:click="onClose", title='cancel')
-                        | cancel
-                    button(v-on:click="onSubmit", title='submit')
-                        | next
+                footer
+                    block dialog_footer
+                        button(v-on:click="onClose", title='cancel')
+                            | cancel
+                        button(v-on:click="onSubmit", title='submit')
+                            | next
 
 </template>
 
@@ -74,16 +75,13 @@
     @import 'cozy-ui'
     [role=dialog]
         @extend $dialog
+        min-height 100vh
+        padding 3em 1em
+        box-sizing border-box
 
         [role=separator]
             cursor pointer
             background-color rgba(78, 91, 105, 0.75)
-
-        .wrapper
-            min-height 100vh
-            padding 3em 1em
-            box-sizing border-box
-
 
         [role=contentinfo]
             overflow initial
