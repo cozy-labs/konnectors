cozy-labs/konnectors

commit 0fb27a86a233a1178c2c02c191567f30dca0d373

Dev tools fixes (#560)
* Dev tools fixes
* Fix split on `:` in dev tools which was preventing from setting a field
to a URL value.
See https://github.com/cozy-labs/konnectors/issues/547#issue-185478387.
* Use JSON description to set fields through the dev tools, which is a
more robust and reliable way to describe the fields values, without
parsing errors due to commas or colons.
* Fix `konnector_hash` and dev tools to be able to use ES6 konnectors as
well as JS folder import as a package.
* Introduce a new script to build server part only, which can be called
from `npm run build:server`.
* Convert dev tools code to ES6, using
[`decaffeinate`](https://github.com/decaffeinate/decaffeinate).
* Fix the dev tools to look for modules in `build` directory rather than
`server` directory directly. This makes it compatible with
ES6/Coffee/JS, at the expense of having to run a `npm run build:server`
before.
* Add a `npm run dev_tools` endpoint to ease launch of dev tools.
* `vendor` is not a Konnector model attribute and then should not be in
the ouput of `--list` command in the dev tools.
* `--change` should not allow one to set fields not declared in the
connector definition.
Fixes #547.
Fixes #553.
* Update .gitignore to ignore Vagrant files

server/controllers/konnectors.coffee
@@ -23,10 +23,18 @@ module.exports =
                     konnector.injectEncryptedFields()
 
                 # Add customView field
-                    konnectorModule = require "../konnectors/#{konnector.slug}"
+                konnectorModule = require(
+                    path.join(
+                        '..',
+                        'konnectors',
+                        konnector.slug
+                    )
+                )
+                if konnectorModule.default?
+                    konnectorModule = konnectorModule.default
 
-                    if konnectorModule.customView?
-                        konnector.customView = konnectorModule.customView
+                if konnectorModule.customView?
+                    konnector.customView = konnectorModule.customView
 
                 req.konnector = konnector
                 next()

.gitignore
@@ -15,3 +15,6 @@ results
 node_modules
 npm-debug.log
 client/public
+
+Vagrantfile
+.vagrant

dev/connector.coffee
@@ -1,115 +1,0 @@
-path = require 'path'
-cozydb = require 'cozydb'
-
-log = require('printit')
-    prefix: 'Konnector dev tool'
-
-
-# Display most important fields of a konnector. It hides the password
-# in case some are stored in the field values of the connector.
-displayKonnector = (konnector) ->
-
-    konnector.removeEncryptedFields()
-    # The fields are in the konnector file
-    konnectorConfig = {}
-    try
-        konnectorConfig = require "../server/konnectors/#{konnector.slug}"
-    catch
-        log.error "Konnector file name and konnector slug do not match.\
-                    Slug is: #{konnector.slug}"
-    log.raw
-        slug: konnector.slug
-        accounts: konnector.accounts
-        fields: konnectorConfig.fields
-        lastSuccess: konnector.lastSuccess
-        lastImport: konnector.lastImport
-        isImporting: konnector.isImporting
-        importInterval: konnector.importInterval
-        importErrorMessage: konnector.importErrorMessage
-    log.lineBreak()
-
-module.exports =
-
-
-    # Run an import to avoid running the full web app.
-    run: (konnectorName, callback) ->
-        Konnector = require '../server/models/konnector'
-        konnectorConfig = require "../server/konnectors/#{konnectorName}"
-        Konnector.get konnectorName, (err, konnector) ->
-            return callback err if err
-            konnector.appendConfigData konnectorConfig
-
-            if not konnector?
-                callback new Error "Konnector not found."
-
-            else
-                log.info "Import starting..."
-                konnector.fieldValues ?= {}
-                konnector.import (err) ->
-                    log.error err if err
-                    callback()
-
-
-    # Allow to change field values of connector. That way the import can be
-    # runned without loading the full web app.
-    # Expected format for fields is an array of string following this syntax:
-    # ["key1:value1", "key2:value2"]
-    change: (konnectorName, fields, callback) ->
-        account = {}
-        for field in fields
-            [key, value] = field.split(':')
-            account[key] = value
-
-        Konnector = require '../server/models/konnector'
-        konnectorMap = require '../server/lib/konnector_hash'
-        Konnector.all (err, konnectors) ->
-            return callback err if err
-
-            konnector = konnectors.find (konnector) ->
-                konnector.slug is konnectorName
-            konnectorMetaData = konnectorMap[konnector.slug]
-
-            if konnector.accounts?.length > 0
-                accounts = konnector.accounts
-                for key, value of account
-                    accounts[0][key] = value
-            else
-                accounts = [account]
-
-            if konnector
-                konnector.updateAttributes accounts: accounts, callback
-            else
-                callback new Error(
-                    "Can't find given konnector (slug expected).")
-
-
-    # Display konnector information stored in the database. It helps debugging
-    # by ensuring that values are properly set.
-    display: (konnectorName, callback) ->
-        Konnector = require '../server/models/konnector'
-        Konnector.all (err, konnectors) ->
-            if konnectorName
-                konnector = konnectors.find (konnector) ->
-                    konnector.slug is konnectorName
-                if konnector
-                    displayKonnector konnector
-                else
-                    log.error "Can't find konnector #{konnectorName}."
-            else
-                konnectors.sort (a, b) ->
-                    if a.slug?
-                        a.slug.localeCompare b.slug
-                    else if a.date?
-                        a.date > b.date
-                    else
-                        a.id > b.id
-                displayKonnector konnector for konnector in konnectors
-            callback()
-
-
-    init: (callback) ->
-        initKonnectors = require '../server/init/konnectors'
-        cozydb.configure {}, null, ->
-            initKonnectors ->
-                callback()
-

dev/connector.js
@@ -1,0 +1,216 @@
+import cozydb from 'cozydb';
+import fs from 'fs';
+import path from 'path';
+
+let baseBuildPath = path.join(
+    path.dirname(fs.realpathSync(__filename)),
+    '../build/'
+);
+
+// Display most important fields of a konnector. It hides the password
+// in case some are stored in the field values of the connector.
+let displayKonnector = function(konnector, log) {
+    konnector.removeEncryptedFields();
+    // The fields are in the konnector file
+    let konnectorConfig = {};
+    try {
+        konnectorConfig = require(
+            path.join(
+                baseBuildPath,
+                '/server/konnectors/',
+                konnector.slug
+            )
+        );
+        if (konnectorConfig.default !== undefined) {
+            konnectorConfig = konnectorConfig.default;
+        }
+    } catch (error) {
+        log.error(`Konnector file name and konnector slug do not match.\
+                    Slug is: ${konnector.slug}`
+        );
+    }
+    log.raw({
+        slug: konnector.slug,
+        accounts: konnector.accounts,
+        fields: konnectorConfig.fields,
+        lastSuccess: konnector.lastSuccess,
+        lastImport: konnector.lastImport,
+        isImporting: konnector.isImporting,
+        importInterval: konnector.importInterval,
+        importErrorMessage: konnector.importErrorMessage
+    });
+    return log.lineBreak();
+};
+
+
+export default {
+    // Run an import to avoid running the full web app.
+    run(konnectorName, callback, log) {
+        let Konnector = require(
+            path.join(
+                baseBuildPath,
+                '/server/models/konnector'
+            )
+        );
+        let konnectorConfig = require(
+            path.join(
+                baseBuildPath,
+                '/server/konnectors/',
+                konnectorName
+            )
+        );
+        if (konnectorConfig.default !== undefined) {
+            konnectorConfig = konnectorConfig.default;
+        }
+        return Konnector.get(konnectorName, function(err, konnector) {
+            if (err) { return callback(err); }
+            konnector.appendConfigData(konnectorConfig);
+
+            if (konnector == null) {
+                return callback(new Error("Konnector not found."));
+
+            } else {
+                log.info("Import starting...");
+                if (konnector.fieldValues == null) { konnector.fieldValues = {}; }
+                return konnector.import(function(err) {
+                    if (err) { log.error(err); }
+                    return callback();
+                });
+            }
+        });
+    },
+
+
+    // Allow to change field values of connector. That way the import can be
+    // runned without loading the full web app.
+    // Expected format for fields is a mapping of fields names to values.
+    change(konnectorName, fields, callback, log) {
+        let account = {};
+        for (var key in fields) {
+            let value = fields[key];
+            if (typeof value === "object") {
+                value = JSON.stringify(value);
+            }
+            account[key] = value;
+        }
+
+        let Konnector = require(
+            path.join(
+                baseBuildPath,
+                '/server/models/konnector'
+            )
+        );
+        let konnectorMap = require(
+            path.join(
+                baseBuildPath,
+                '/server/lib/konnector_hash'
+            )
+        );
+        return Konnector.all(function(err, konnectors) {
+            if (err) { return callback(err); }
+
+            let konnector = konnectors.find(konnector => konnector.slug === konnectorName);
+            let konnectorMetaData = konnectorMap[konnector.slug];
+
+            if (konnector.accounts && konnector.accounts.length > 0) {
+                var { accounts } = konnector;
+                for (key in account) {
+                    let fields = konnector.getFields();
+                    if (fields[key] === undefined) {
+                        return callback(new Error(
+                            "Can't set fields which are not in the konnector fields list: " + key)
+                        );
+                    }
+                    let value = account[key];
+                    accounts[0][key] = value;
+                }
+            } else {
+                var accounts = [account];
+            }
+
+            if (konnector) {
+                return konnector.updateAttributes({accounts}, callback);
+            } else {
+                return callback(new Error(
+                    "Can't find given konnector (slug expected).")
+                );
+            }
+        });
+    },
+
+
+    // Display konnector information stored in the database. It helps debugging
+    // by ensuring that values are properly set.
+    display(konnectorName, callback, log) {
+        let Konnector = require(
+            path.join(
+                baseBuildPath,
+                '/server/models/konnector'
+            )
+        );
+        return Konnector.all(function(err, konnectors) {
+            if (konnectorName) {
+                var konnector = konnectors.find(konnector => konnector.slug === konnectorName);
+                if (konnector) {
+                    displayKonnector(konnector, log);
+                } else {
+                    log.error(`Can't find konnector ${konnectorName}.`);
+                }
+            } else {
+                konnectors.sort(function(a, b) {
+                    if (a.slug != null) {
+                        return a.slug.localeCompare(b.slug);
+                    } else if (a.date != null) {
+                        return a.date > b.date;
+                    } else {
+                        return a.id > b.id;
+                    }
+                });
+                for (var konnector of konnectors) { displayKonnector(konnector, log); }
+            }
+            return callback();
+        });
+    },
+
+
+    /**
+     * Initialize connectors
+     */
+    init(callback, log) {
+        let initKonnectors = require(
+            path.join(
+                baseBuildPath,
+                '/server/init/konnectors'
+            )
+        );
+        return cozydb.configure(
+            {
+                modelsPath: path.join(baseBuildPath, '/server/models')
+            },
+            null,
+            () => initKonnectors(() => callback())
+        );
+    },
+
+    /**
+     * Deinitialize connectors
+     */
+    deinit(konnectorName, callback, log) {
+        let Konnector = require(
+            path.join(
+                baseBuildPath,
+                '/server/models/konnector'
+            )
+        );
+        return Konnector.all(function(err, konnectors) {
+            if (err) { return log.error(err); }
+
+            if (konnectorName) {
+                let konnector = konnectors.find(konnector => konnector.slug === konnectorName);
+                return konnector.destroy(callback);
+            } else {
+                konnectors.map(konnector => konnector.destroy(callback));
+            }
+        });
+    }
+};

dev/data.coffee
@@ -1,87 +1,0 @@
-path = require 'path'
-moment = require 'moment'
-cozydb = require 'cozydb'
-Table = require 'cli-table'
-log = require('printit')
-    prefix: 'Konnector dev tool'
-
-
-
-getModel = (modelName) ->
-    try
-        Model = require path.join __dirname, "../server/models/#{modelName}"
-        return Model
-    catch
-        log.error "Cannot find given model."
-        process.exit 1
-
-
-module.exports =
-
-    # Delete all instance of given model in database.
-    # It allows to clean the database after one or several imports. It makes
-    # things clearer for debugging.
-    delete: (modelName, callback) ->
-        Model = getModel modelName
-        Model.requestDestroy 'byDate', (err) ->
-            if err
-                Model.requestDestroy 'all', (err) ->
-                    log.error.err if err
-                    callback err
-            else
-                callback()
-
-
-    # Display in a table all instances of a given listed in database.
-    # It allow to select which fields should be displayed and set the width
-    # of columns via `columns` and `width` fields.
-    # It's useful to see what was imported in the database after an import.
-    show: (opts, callback) ->
-        {modelName, columns, widths} = opts
-        Model = getModel modelName
-        head = ['date', 'vendor']
-        cols = []
-
-        if columns
-            cols = columns.toString().split ','
-            head = cols
-
-        if widths
-            widths = widths.toString().split ','
-            widths = widths.map (width) ->
-                parseInt width
-            colWidths = widths
-        else
-            colWidths = [13, 20]
-
-        table = new Table
-            head: head
-            colWidths: colWidths
-
-        Model.all (err, models) ->
-            models.sort (a, b) ->
-                if a.slug?
-                    a.slug.localeCompare b.slug
-                else if a.date?
-                    a.date > b.date
-                else
-                    a.id > b.id
-
-            for model in models
-                if cols.length is 0
-                    row = [
-                        moment(model.date).format('YYYY-MM-DD')
-                        model.vendor or ''
-                    ]
-                else
-                    row = []
-                values = cols.map (col) ->
-                    return model[col] or ''
-
-                row = row.concat values
-                table.push row
-
-            console.log table.toString()
-            console.log "#{models.length} rows"
-            callback()
-

dev/data.js
@@ -1,0 +1,110 @@
+import Table from 'cli-table';
+import cozydb from 'cozydb';
+import fs from 'fs';
+import moment from 'moment';
+import path from 'path';
+
+let baseBuildPath = path.join(
+    path.dirname(fs.realpathSync(__filename)),
+    '../build/'
+);
+
+
+let getModel = function(modelName, log) {
+    try {
+        let Model = require(
+            path.join(
+                baseBuildPath,
+                '/server/models/',
+                modelName
+            )
+        );
+        return Model;
+    } catch (error) {
+        log.error("Cannot find given model.");
+        return process.exit(1);
+    }
+};
+
+
+export default {
+
+    // Delete all instance of given model in database.
+    // It allows to clean the database after one or several imports. It makes
+    // things clearer for debugging.
+    delete(modelName, callback, log) {
+        let Model = getModel(modelName, log);
+        return Model.requestDestroy('byDate', function(err) {
+            if (err) {
+                return Model.requestDestroy('all', function(err) {
+                    if (err) { log.error.err; }
+                    return callback(err);
+                });
+            } else {
+                return callback();
+            }
+        });
+    },
+
+
+    // Display in a table all instances of a given listed in database.
+    // It allow to select which fields should be displayed and set the width
+    // of columns via `columns` and `width` fields.
+    // It's useful to see what was imported in the database after an import.
+    show(opts, callback, log) {
+        let {modelName, columns, widths} = opts;
+        let Model = getModel(modelName, log);
+        let head = ['date', 'vendor'];
+        let cols = [];
+
+        if (columns) {
+            cols = columns.toString().split(',');
+            head = cols;
+        }
+
+        if (widths) {
+            widths = widths.toString().split(',');
+            widths = widths.map(width => parseInt(width));
+            var colWidths = widths;
+        } else {
+            var colWidths = [13, 20];
+        }
+
+        let table = new Table({
+            head,
+            colWidths
+        });
+
+        return Model.all(function(err, models) {
+            models.sort(function(a, b) {
+                if (a.slug != null) {
+                    return a.slug.localeCompare(b.slug);
+                } else if (a.date != null) {
+                    return a.date > b.date;
+                } else {
+                    return a.id > b.id;
+                }
+            });
+
+            for (let model of models) {
+                if (cols.length === 0) {
+                    var row = [
+                        moment(model.date).format('YYYY-MM-DD'),
+                        model.vendor || ''
+                    ];
+                } else {
+                    var row = [];
+                }
+                let values = cols.map(col => model[col] || '');
+
+                var row = row.concat(values);
+                table.push(row);
+            }
+
+            console.log(table.toString());
+            console.log(`${models.length} rows`);
+            return callback();
+        });
+    }
+};
+

dev/index.js
@@ -1,0 +1,4 @@
+import connector from './connector';
+import data from './data';
+
+export { connector, data };

dev_tools.coffee
@@ -1,102 +1,0 @@
-program = require 'commander'
-data = require './dev/data'
-connector = require './dev/connector'
-log = require('printit')
-    prefix: 'Konnector dev tool'
-
-
-program
-    .usage('[options]')
-    .option('-a, --status [connector]', 'Show given connector data')
-    .option('-i, --import <connector>', 'Run import for given konnector.')
-    .option('-k, --change <connector>',
-            'Change field values for given konnector.')
-    .option('-f, --fields <fieldValues>', 'New value to set as fields.')
-    .option('-n, --init', 'Init connectors metadata.')
-    .option('-s, --show <docType>', 'Show documents for given model.')
-    .option('-d, --delete <docType>', 'Delete documents for given model.')
-    .option('-c, --columns <columns>', 'Set column names for data table.')
-    .option('-w, --widths <widths>', 'Set widths for data table columns.')
-    .option('-l, --list', 'List the slugs of all available konnectors')
-    .parse(process.argv)
-
-
-if program.status
-    konnectorName = program.status
-
-    if typeof(konnectorName) is 'boolean'
-        connector.display null, ->
-    else
-        connector.display konnectorName, ->
-
-
-else if program.change
-
-    if program.fields
-        konnectorName = program.change
-        fields = program.fields.split(',')
-        log.info "Changing fields for #{konnectorName} with #{fields}."
-        connector.change konnectorName, fields, (err) ->
-            if err
-                log.error "Failed to change fields."
-                log.error err
-            else
-                log.info """
-Fields were successfully changed for #{konnectorName}
-Note: only fields described in the connector file are stored.
-"""
-
-    else
-        log.error "Field values are required (see --fields option)."
-
-
-else if program.import
-    konnectorName = program.import
-    log.info "Running import for #{konnectorName}..."
-
-    connector.run konnectorName, (err) ->
-        if err
-            log.error "An error occured while trying to run the import."
-            log.error err.message
-        else
-            log.info "Import is finished."
-
-
-else if program.init
-
-    log.info 'Start initializing connectors...'
-    connector.init ->
-        log.info 'Init done.'
-
-
-else if program.show or program.delete
-
-    if program.delete
-        modelName = program.delete
-        data.delete modelName, (err) ->
-            if err
-                log.info 'Destroying models failed.'
-            else
-                log.info 'All models were destroyed.'
-
-    else if program.show
-        modelName = program.show
-        opts =
-            modelName: modelName,
-            columns: program.columns,
-            widths: program.widths
-        data.show opts, ->
-            log.lineBreak()
-
-else if program.list
-    opts =
-        modelName: 'konnector'
-        columns: 'slug,vendor'
-        widths: '20,20'
-    data.show opts, ->
-        log.lineBreak()
-
-else
-    program.outputHelp()
-    process.exit 0
-

commit cf603e46342853ecbda5e064dc5aeb0770b6ea7b

Fix crash on konnector page load, introduced with #560

server/controllers/konnectors.coffee
@@ -1,3 +1,4 @@
+path = require 'path'
 Konnector = require '../models/konnector'
 konnectorHash = require '../lib/konnector_hash'
 handleNotification = require '../lib/notification_handler'