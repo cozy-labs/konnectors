98f3f00cc1daa1fb2b7625dc11c3f5c070718d12
Noëlie Andrieu <noelieandrieu@gmail.com>
2016-11-16 17:03:15 +0100

    [fix] missing dialog.title

--- client/app/app.vue
+++ client/app/app.vue
@@ -63,6 +63,8 @@
     const Dialogs = [{
         id: 'dialog-1',
 
+        title: "dialog title",
+
         headerStyles: {
             'background-image': `url(header.png)`,
             'height': '100px'
