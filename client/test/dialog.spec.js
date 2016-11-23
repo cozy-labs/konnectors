094cf7bdbc09d65c59b80623a8caa9c144abd191
Noëlie Andrieu <noelieandrieu@gmail.com>
2016-11-23 10:59:27 +0100

    [test] add dialog.vue backbone

--- client/test/dialog.spec.js
+++ client/test/dialog.spec.js
@@ -13,170 +13,219 @@ describe('Dialogs', () => {
   const vm
   const dialogs
 
-  describe('data()', () => {
-    describe('`dialogs` should be equal to [] (defaut)', () => {
-      expect(App.data().dialogs).toBe([])
-    })
+  describe('App.vue', () => {
 
-    it('`notifications` should be equal to []', () => {
-      expect(App.data().notifications).toBe([])
-    })
+    describe('data', () => {
+      describe('`dialogs` should be equal to []', () => {
+        expect(App.data().dialogs).toBe([])
+      })
 
-    it('`dialogsQuery` should return `\'\'` for `dialogs=[]`', () => {
-      expect(App.data().dialogs).toBe([])
-      expect(App.computed.dialogsQuery.get()).toBe('')
-    })
-  })
+      it('`notifications` should be equal to []', () => {
+        expect(App.data().notifications).toBe([])
+      })
 
-
-  describe('`updateDialogs()` ', () => {
-
-    beforeEach(() => {
-      vm = new Vue({
-        template: '<div><test></test></div>',
-        components: {
-          'test': App
-        }
-      }).$mount()
-
-      dialogs = 'truc'
-      vm.updateDialogs({ query: dialogs })
-    })
-
-    afterEach(() => {
-      vm.$destroy()
+      it('`dialogsQuery` should return `\'\'` for `dialogs=[]`', () => {
+        expect(App.data().dialogs).toBe([])
+        expect(App.computed.dialogsQuery.get()).toBe('')
+      })
     })
 
 
-    it('should update `vm.dialogs', () => {
-      expect(vm.dialogs).toBe([dialogs])
-    })
+    describe('methods', () => {
+      describe('`updateDialogs()` ', () => {
+
+        beforeEach(() => {
+          vm = new Vue({
+            template: '<div><test></test></div>',
+            components: {
+              'test': App
+            }
+          }).$mount()
+
+          dialogs = 'truc'
+          vm.updateDialogs({ query: dialogs })
+        })
+
+        afterEach(() => {
+          vm.$destroy()
+        })
 
 
-    it('should update `vm.query`', () => {
-      expect(vm.dialogsQuery).toBe(`dialogs=${dialogs}`)
-    })
+        it('should update `vm.dialogs', () => {
+          expect(vm.dialogs).toBe([dialogs])
+        })
 
 
-    it('should update `vm.$router.currentRoute`', () => {
-      expect(this.$router.currentRoute.query).toBe(vm.dialogsQuery)
-    })
-
-  })
+        it('should update `vm.query`', () => {
+          expect(vm.dialogsQuery).toBe(`dialogs=${dialogs}`)
+        })
 
 
-  describe('`onOpenDialog` ', () => {
-    it('this.dialogQuery should be equal to `dialogs=plop`', () => {
-      // prévoir plusieurs ajouts à la suite;
-      // ne pas avoir plusieurs fois le même param dans la query
-    })
-  })
+        it('should update `vm.$router.currentRoute`', () => {
+          expect(this.$router.currentRoute.query).toBe(vm.dialogsQuery)
+        })
+
+      })
 
 
-  describe('`onCloseDialog` ', () => {
-    it('should call `onOpenNotif`', () => {
-
-    })
-  })
-
-
-  describe('`onSuccessDialog` ', () => {
-    it('should close current dialog', () => {
-
-    })
+      describe('`onOpenDialog` ', () => {
+        it('this.dialogQuery should be equal to `dialogs=plop`', () => {
+          // prévoir plusieurs ajouts à la suite;
+          // ne pas avoir plusieurs fois le même param dans la query
+        })
+      })
 
 
-    it('should redirect to config.success', () => {
-
-    })
-  })
-
-
-  describe('`onErrorDialog` ', () => {
-    it('should call onOpenNotif with `err` and `id` args', () => {
-
-    })
-  })
-
-
-  describe('`onOpenNotif` ', () => {
-      describe('`notifications` ', () => {
-        it('should be equal to `[{ err, label, dialog }]`', () => {
+      describe('`onCloseDialog` ', () => {
+        it('should call `onOpenNotif`', () => {
 
         })
       })
-  })
 
 
-  describe('`onCloseNotif` ', () => {
-    describe('`notifications` ', () => {
-      it('shouldnt have values from `dialog` anymore', () => {
+      describe('`onSuccessDialog` ', () => {
+        it('should close current dialog', () => {
 
-      })
-    })
-  })
+        })
 
 
-  describe('Use Cases', () => {
+        it('should redirect to config.success', () => {
 
-    describe('Routing', () => {
-      beforeEach(() => {
-        vm = new Vue({
-          template: '<div><test></test></div>',
-          components: {
-            'test': App
-          }
-        }).$mount()
-      })
-
-      afterEach(() => {
-        vm.$destroy()
+        })
       })
 
 
-      describe('location/?dialogs=plop', () => {
-          beforeEach(() => {
-            dialogs = 'plop'
-            this.$router.push({ query: { dialogs: dialogs } })
-          })
+      describe('`onErrorDialog` ', () => {
+        it('should call onOpenNotif with `err` and `id` args', () => {
+
+        })
+      })
 
 
-          it('should update `dialogs`', () => {
-            expect(vm.dialogs).toBe([dialogs])
-          })
+      describe('`onOpenNotif` ', () => {
+          describe('`notifications` ', () => {
+            it('should be equal to `[{ err, label, dialog }]`', () => {
 
-
-          it('should update `dialogsQuery`', () => {
-            expect(vm.dialogsQuery).toBe(`dialogs=${dialogs}`)
+            })
           })
       })
-    })
 
 
-    describe('Click on `showDialog` button', () => {
-      it('should open `dialogVue`', () => {
+      describe('`onCloseNotif` ', () => {
+        describe('`notifications` ', () => {
+          it('shouldnt have values from `dialog` anymore', () => {
 
+          })
+        })
       })
     })
 
 
-    describe('Click on `closeDialog` button', () => {
-      it('should close `dialogVue`', () => {
+    describe('Use Cases', () => {
 
+      describe('Routing', () => {
+        beforeEach(() => {
+          vm = new Vue({
+            template: '<div><test></test></div>',
+            components: {
+              'test': App
+            }
+          }).$mount()
+        })
+
+        afterEach(() => {
+          vm.$destroy()
+        })
+
+
+        describe('location/?dialogs=plop', () => {
+            beforeEach(() => {
+              dialogs = 'plop'
+              this.$router.push({ query: { dialogs: dialogs } })
+            })
+
+
+            it('should update `dialogs`', () => {
+              expect(vm.dialogs).toBe([dialogs])
+            })
+
+
+            it('should update `dialogsQuery`', () => {
+              expect(vm.dialogsQuery).toBe(`dialogs=${dialogs}`)
+            })
+        })
+      })
+
+
+      describe('Click on `showDialog` button', () => {
+        it('should open `dialogVue`', () => {
+
+        })
+      })
+
+
+      describe('Click on `closeDialog` button', () => {
+        it('should close `dialogVue`', () => {
+
+        })
+      })
+
+
+      describe('Click on `success` button', () => {
+        it('should display a `success` notif', () => {
+
+        })
+      })
+
+
+      describe('Click on `error` button', () => {
+        it('should display a `error` notif', () => {
+
+        })
       })
     })
 
 
-    describe('Click on `success` button', () => {
-      it('should display a `success` notif', () => {
+    describe('Dialog.vue', () => {
 
+      describe('props', () => {
+        it('should have `id` property', () => {
+
+        })
+
+        it('should have `headerStyles` property', () => {
+
+        })
+
+        it('should have `hub` property', () => {
+
+        })
       })
-    })
 
 
-    describe('Click on `error` button', () => {
-      it('should display a `error` notif', () => {
+      describe('computed', () => {
+        describe('closeURL', () => {
 
+        })
+
+        describe('closeQuery', () => {
+
+        })
+      })
+
+
+      describe('methods', () => {
+        describe('close', () => {
+
+        })
+
+        describe('error', () => {
+
+        })
+
+        describe('success', () => {
+
+        })
       })
     })
   })
