5bad038a6a213911cd9ae4a91a907b54967ded84
Cedric Patchane Lacane <cedric@cozycloud.cc>
2016-11-27 00:19:20 +0100

    [feat] Use the generic list item component in the category part

--- client/app/components/category_list.vue
+++ client/app/components/category_list.vue
@@ -3,19 +3,42 @@
 
     block content
         h1 {{ 'my_accounts category title' | t }}
-        article {{ 'my_accounts coming_soon' | t }}
-
-        ul
-            li
-                button(v-on:click="openDialog") open dialog
+        div.konnectors-list
+            konnector-item(
+                v-for="konnector in konnectorsList",
+                v-if="!konnector.accounts.length",
+                :title="konnector.name",
+                :subtitle="konnector.category",
+                :backgroundCSS="konnector.color.css",
+                :enableDefaultIcon="true",
+                :iconName="konnector.slug",
+                link="#")
 </template>
 
 <script>
+    import konnectorItemComponent from './list_item'
+
     export default {
-      methods: {
-          openDialog () {
-              this.$emit('open-dialog', 'dialog-1')
-          }
-      }
+        data: function () {
+            return {
+                konnectorsList: window.initKonnectors
+            }
+        },
+
+        components: {
+            'konnectorItem': konnectorItemComponent
+        },
+
+        methods: {
+            openDialog () {
+                this.$emit('open-dialog', 'dialog-1')
+            }
+        },
     }
 </script>
+
+<style lang="stylus">
+    .konnectors-list
+        display         flex
+        flex-wrap       wrap
+</style>
