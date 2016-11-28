cd13bece2aef061523ab4cbddceeb436699380b5
Antoine BOSSY <ZeHiro@users.noreply.github.com>
2016-11-28 21:28:34 +0100

    Fix PATH for all scripts (#600)

--- scripts/lint.sh
+++ scripts/lint.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+PATH="./node_modules/.bin:$PATH"
 
 ./node_modules/.bin/coffeelint -f coffeelint.json -r --color=always . &&\
 ./node_modules/.bin/standard  "server/konnectors/*.js"
