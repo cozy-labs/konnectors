db1760ef68908d09376c0af60c847dcaf3c061ae
Raphaël Rougeron <goldoraf@gmail.com>
2016-12-05 12:41:48 +0100

    [feature] Filter by category

--- client/app/components/sidebar.jsx
+++ client/app/components/sidebar.jsx
@@ -2,40 +2,48 @@ import { h, render } from 'preact'
 import { Link, withRouter } from 'react-router'
 import { translate } from '../plugins/preact-polyglot'
 
-const Sidebar = ({ t, categories, router }) => (
-    <aside>
-        <h4>{t('my_accounts title')}</h4>
-        <ul role="navigation">
-            <li>
-                <Link to="/" activeClassName="router-link-active">
-                    <svg><use xlinkHref={require('../assets/sprites/icon-discovery.svg')}/></svg>
-                    {t('my_accounts discovery title')}
-                </Link>
-            </li>
-            <li>
-                <Link to="/category/all" activeClassName="router-link-active">
-                    <svg><use xlinkHref={require('../assets/sprites/icon-category.svg')}/></svg>
-                    {t('my_accounts category title')}
-                </Link>
-                
-            </li>
-            <ul>
-                {categories.map(category => (
-                    <li>
-                        <Link to={`/category/${category}`} activeClassName="router-link-active">
-                            {t(`${category} category`)}
-                        </Link>
-                    </li>
-                ))}
+const Sidebar = ({ t, categories, router }) => {
+    let isCategoryView = router.location.pathname.match(/^\/category/) !== null
+    return (
+        <aside>
+            <h4>{t('my_accounts title')}</h4>
+            <ul role="navigation">
+                <li>
+                    <Link to="/" activeClassName="router-link-active">
+                        <svg><use xlinkHref={require('../assets/sprites/icon-discovery.svg')}/></svg>
+                        {t('my_accounts discovery title')}
+                    </Link>
+                </li>
+                <li>
+                    <Link to="/category/all"
+                        className={isCategoryView ? 'router-link-active': ''}>
+                        <svg><use xlinkHref={require('../assets/sprites/icon-category.svg')}/></svg>
+                        {t('my_accounts category title')}
+                    </Link>
+                    {isCategoryView &&
+                        <ul class="submenu">
+                            <Link to="/category/all" activeClassName="router-link-active">
+                                {t('all category')}
+                            </Link>
+                            {categories.map(category => (
+                                <li>
+                                    <Link to={`/category/${category}`} activeClassName="router-link-active">
+                                        {t(`${category} category`)}
+                                    </Link>
+                                </li>
+                            ))}
+                        </ul>
+                    }
+                </li>
+                <li>
+                    <Link to="/connected" activeClassName="router-link-active">
+                        <svg><use xlinkHref={require('../assets/sprites/icon-connected.svg')}/></svg>
+                        {t('my_accounts connected title')}
+                    </Link>
+                </li>
             </ul>
-            <li>
-                <Link to="/connected" activeClassName="router-link-active">
-                    <svg><use xlinkHref={require('../assets/sprites/icon-connected.svg')}/></svg>
-                    {t('my_accounts connected title')}
-                </Link>
-            </li>
-        </ul>
-    </aside>
-)
+        </aside>
+    )
+}
 
 export default translate()(withRouter(Sidebar))
