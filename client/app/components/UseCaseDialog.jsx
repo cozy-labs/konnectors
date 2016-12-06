22f019a17e63b3af13f3a3d41be055fb10c46363
Cedric Patchane Lacane <cedric@cozycloud.cc>
2016-12-06 17:34:31 +0100

    [feat] Add use case dialog main content

--- client/app/components/UseCaseDialog.jsx
+++ client/app/components/UseCaseDialog.jsx
@@ -2,23 +2,37 @@
 import { h } from 'preact'
 import { translate } from '../plugins/preact-polyglot'
 import { withRouter } from 'react-router'
+import AccountItem from './AccountItem'
 
 const CloseButton = withRouter(({ router }) => (
   <button role='close' onClick={router.goBack}>Close</button>
 ))
 
-const UseCaseDialog = ({ t, item }) => (
+const UseCaseDialog = ({ t, item, context }) => (
   <div role='dialog'>
     <div class='wrapper'>
       <div role='contentinfo'>
-        <header>
+        <header style={{background:
+          `center/100% url(${require(`../contexts/${context}/assets/img/${item.figure}`)})`}}
+        >
           <CloseButton />
-          <h3>{item.slug}</h3>
-          <main>
-            <p>Foo</p>
-          </main>
-          <footer />
         </header>
+        <main>
+          <h3>{t(`use-case ${item.slug} title`)}</h3>
+          <p>{t(`use-case ${item.slug} description`)}</p>
+          <div class='accounts-list'>
+            {item.accounts.map(a =>
+              <AccountItem
+                title={a.name}
+                subtitle={t(a.category + ' category')}
+                iconName={a.slug}
+                slug={a.slug}
+                enableDefaultIcon
+                backgroundCSS={a.color.css}
+              />
+            )}
+          </div>
+        </main>
       </div>
     </div>
   </div>
