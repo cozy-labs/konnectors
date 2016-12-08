9dad6cf5d2d09013a08a479cb425aec71c29d67f
Cedric Patchane Lacane <cedric@cozycloud.cc>
2016-12-08 17:17:56 +0100

    [feat] Better and simpler accounts helper

--- client/app/lib/accountsHelper.js
+++ client/app/lib/accountsHelper.js
@@ -9,30 +9,18 @@ export default {
       if (fields) {
         // check calendar field
         if (fields.calendar && !fields.calendar.default) {
-          if (typeof fields.calendar !== 'object') {
-            fields.calendar = {}
-          }
           fields.calendar.default = a.name
-          if (!fields.calendar.type) fields.calendar.type = 'text'
         }
 
         // check folderPath field
         if (fields.folderPath && !fields.folderPath.default) {
-          if (typeof fields.folderPath !== 'object') {
-            fields.folderPath = {}
-          }
           // <my_accounts> will be replaced by the localized app name
           fields.folderPath.default = `<my_accounts>/${a.name}`
-          if (!fields.folderPath.type) fields.folderPath.type = 'text'
         }
 
         // check frequency field
         if (fields.frequency && !fields.frequency.default) {
-          if (typeof fields.frequency !== 'object') {
-            fields.frequency = {}
-          }
           fields.frequency.default = 'weekly'
-          if (!fields.frequency.type) fields.frequency.type = 'text'
         }
       }
     })
