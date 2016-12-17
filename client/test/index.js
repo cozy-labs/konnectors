7bf2c91c65d05a03fc4e5a85d73332208af326a0
Raphaël Rougeron <goldoraf@gmail.com>
2016-12-17 17:06:05 +0100

    [tech] Added sample Jest test

--- client/test/index.js
+++
@@ -1,9 +0,0 @@
-var testsContext = require.context('.', true, /spec$/)
-testsContext.keys().forEach(function (path) {
-  try {
-    testsContext(path)
-  } catch (err) {
-    console.error('[ERROR] WITH SPEC FILE: ', path)
-    console.error(err)
-  }
-})
