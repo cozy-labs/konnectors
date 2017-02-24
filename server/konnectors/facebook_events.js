7b9e747636661daea92d4573af97b6064b60b85b
jacquarg <guillaume.jacquart@hoodbrains.com>
2017-02-24 16:12:06 +0100

    Allow select calendar, and fix facebook konnector fix #420

--- server/konnectors/facebook_events.js
+++ server/konnectors/facebook_events.js
@@ -26,8 +26,6 @@ const oAuthProxyUrl = getOAuthProxyUrl()
 const connector = module.exports = baseKonnector.createNew({
   name: 'Facebook Events',
   slug: 'facebook_events',
-  customView: `<a href=${oAuthProxyUrl} target="_blank" role="button">` +
-    `<%t konnector facebook_events connect %></a>`,
 
   category: 'social',
   color: {
@@ -35,13 +33,13 @@ const connector = module.exports = baseKonnector.createNew({
     css: '#395185'
   },
 
+  connectUrl: getOAuthProxyUrl(),
   fields: {
     accessToken: {
-      type: 'text'
+      type: 'hidden'
     },
     calendar: {
-      type: 'text',
-      advanced: true
+      type: 'text'
     }
   },
 
@@ -104,30 +102,17 @@ function saveTokenInKonnector (requiredFields, entries, data, callback) {
   const Konnector = require('../models/konnector')
   /* eslint-enable global-require */
   // TODO: should work:
-  // Konnector.get(connector.slug, function(err, konnector) {
-  Konnector.all((err, konnectors) => {
+  Konnector.get(connector.slug, (err, konnector) => {
     if (err) {
       connector.logger.error(`Can't fetch konnector instances: ${err.msg}`)
-      return callback(err)
-    }
-    let konnector = null
-    try {
-      konnector = konnectors.filter(k => k.slug === connector.slug)[0]
-
-      // Find which account we are using now.
-      const currentAccount = konnector.accounts.filter(account =>
-        account.accessToken === requiredFields.accessToken)[0]
-
-      currentAccount.accessToken = data.accessToken
-    } catch (e) {
-      connector.logger.error("Can't fetch konnector instances")
-      return callback(e)
+      return callback('internal error')
     }
 
-    return konnector.updateAttributes({ accounts: konnector.accounts },
-      callback)
-  })
+    requiredFields.accessToken = data.accessToken;
+    konnector.updateFieldValues({ accounts: [requiredFields] }, callback);
+  });
 }
+
 
 function downloadData (requiredFields, entries, data, next) {
   connector.logger.info('Downloading events data from Facebook...')
